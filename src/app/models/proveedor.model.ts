29988c1735a66118b91e22f44e275ec1b1ad0799
lolo2208 <lorenzogog@gmail.com>
2023-10-15 09:50:37 -0500

    primer commit

--- src/app/models/proveedor.model.ts
+++ src/app/models/proveedor.model.ts
@@ -1,2 +1,15 @@
+import { Pais } from "./pais.model";
+import { Usuario } from "./usuario.model";
+
 export class Proveedor {
+    /* idProveedor?:number;
+    razonsocial?:string;
+    ruc?:string;
+    direccion?:string;
+    celular?:string;
+    contacto?:string;
+    pais?:Pais;
+    tipoProveedor?:
+    usuarioRegistro?:Usuario;
+    usuarioActualiza?:Usuario; */
 }
