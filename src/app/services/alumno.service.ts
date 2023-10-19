4eba573863039b281cd0bc13b1d8ce1e0dc6390b
mgct30 <mgct30@gmail.com>
2023-10-19 00:00:20 -0500

    Registra Alumno completado

--- src/app/services/alumno.service.ts
+++ src/app/services/alumno.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AppSettings } from '../app.settings';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Alumno } from '../models/alumno.model';
+
 
 const baseUrlAlumno = AppSettings.API_ENDPOINT+ '/alumno';
 
@@ -8,5 +12,10 @@ const baseUrlAlumno = AppSettings.API_ENDPOINT+ '/alumno';
 })
 export class AlumnoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  registrar(data:Alumno):Observable<any>{
+    return this.http.post(baseUrlAlumno, data);
+  }
+
 }
